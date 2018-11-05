9d7811287080ddfbc8eca5ad6af36062677ed8f4
Nishii Yuuta <z00s600076@rfg.rfgad.ricoh.com>
2018-11-05 14:14:26 +0900

    fixed docker environment

--- pandaApp/public/javascripts/admin.js
+++ pandaApp/public/javascripts/admin.js
@@ -1,9 +1,19 @@
-function selectSpeaker (speaker) {
+function selectSpeaker(speaker) {
   var xhr = new XMLHttpRequest();
-  var url = location.href.replace("admin","");
+  var url = location.href.replace("admin", "");
   xhr.open("post", url + "speaker");
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhr.send('speaker=' + speaker);
   xhr.addEventListener("load", function() {
+    document.getElementById('current-speaker').textContent = speaker;
   }, false);
 }
+
+var xhr = new XMLHttpRequest();
+var url = location.href.replace("admin", "");
+xhr.open("get", url + "speaker");
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xhr.send();
+xhr.addEventListener("load", function() {
+  document.getElementById('current-speaker').textContent = this.response;
+}, false);
--- pandaApp/routes/speaker.js
+++ pandaApp/routes/speaker.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 var speaker = "0";
 
-/* GET speaker listing. */
+/* POST speaker listing. */
 router.post('/', function(req, res, next) {
   speaker = req.body.speaker;
   res.send();
 });
 
-/* POST speaker listing. */
+/* GET speaker listing. */
 router.get('/', function(req, res, next) {
   res.send(speaker);
 });
